f41b578d7337aa921e75440552ec292fd2187d76
Eddie He <erheng.he@m.scnu.edu.cn>
2025-01-14 07:47:08 +0800

    fix: config value

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,12 +13,12 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/banzhuren-notifier-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'eddiehe99', // Usually your GitHub org/user name.
+  projectName: 'banzhuren-notifier-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
