9a3efe97ba6942175b5ec2e3e3d38d89af6c69ad
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-02-10 01:28:33 +0800

    feat: auto deploy

--- src/components/LanRequirement.tsx
+++ src/components/LanRequirement.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Admonition from '@theme/Admonition';
 
-export default function LanRequirement() {
+export default function LanRequirement(): React.ReactElement {
     return (
         <div>
-            <Admonition type="info" title="必须">
+            <Admonition type="info" title="必须" children={''}>
                 教室的电脑和班主任的电脑，都【必须】能使用共享。
             </Admonition>
-            <Admonition type="info" title="必须">
+            <Admonition type="info" title="必须" children={''}>
                 班主任的电脑【必须】保持开机。
             </Admonition>
         </div>
