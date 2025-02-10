013539c6873c782144d2f118971b9880e62cbe30
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-02-10 20:18:57 +0800

    fix: monthly deploy embedding the latest python release version

---
+++ src/components/LatestPythonDownloadLink.jsx
@@ -0,0 +1,22 @@
+// src/components/LatestPythonDownload.js
+import React, { useEffect, useState } from 'react';
+import Link from '@docusaurus/Link';
+import latestPythonReleaseVersionJson from '../../latest-python-release-version.json';
+
+const LatestPythonDownload = () => {
+    const [latestPythonReleaseVersion, setlatestPythonReleaseVersion] = useState(latestPythonReleaseVersionJson.latestPythonReleaseVersion);
+
+    useEffect(() => {
+        setlatestPythonReleaseVersion(latestPythonReleaseVersionJson.latestPythonReleaseVersion);
+    }, [latestPythonReleaseVersionJson.latestPythonReleaseVersion]);
+
+    const downloadUrl = `https://www.python.org/ftp/python/${latestPythonReleaseVersion}/python-${latestPythonReleaseVersion}-amd64.exe`;
+
+    return (
+        <Link href={downloadUrl} target="_blank" rel="noopener noreferrer">
+            点我！我是 Python ({latestPythonReleaseVersion}) 下载链接。
+        </Link>
+    );
+};
+
+export default LatestPythonDownload;
